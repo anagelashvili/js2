2ba03224f31df5e3762fcd80ae291b531f7dad11
anagelashvili <gelashvilianna564@gmail.com>
2024-07-21 21:18:58 +0400

    aa

---
+++ example.js
@@ -0,0 +1,94 @@
+///1///
+
+// let variable = 100;
+
+// if (variable < 50) {
+//     console.log("less than 50");
+// } else if (variable > 20) {
+//     console.log("greater than 20");
+// } else {
+//     console.log("an error");
+// }
+
+//2//
+
+
+// let variable = "მარიამ";
+
+// if (variable === "მარიამ") {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+
+//3//
+// let variable = "მარიამ";
+
+// switch (variable) {
+//     case "მარიამ":
+//         console.log("true");
+//         break;
+//     default:
+//         console.log("false");
+// }
+
+
+
+///4///
+// for (let i = 5; i <= 100; i++) {
+//     console.log(i);
+// }
+
+///5///
+// let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+// for (let i = 0; i < array1.length; i++) {
+//     let number = array1[i];
+//     if (number > 0 && number < 10) {
+//         console.log(number);
+//     }
+// }
+
+
+///6///
+// let user2 = {
+//     name: 'anna',
+//     age: 20,
+//     studentstatus: 'active'
+// };
+
+// if (user2.age < 18 && user2.studentstatus === 'active') {
+//     console.log("hello user");
+// } else if (user2.name === 'levani') {
+//     console.log("hello levani");
+// } else if (user2.studentstatus === 'active' || user2.age < 25) {
+//     console.log("hello anna");
+// } else {
+//     console.log("error");
+// }
+
+
+///7///
+// let array6 = [2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10];
+
+// array6.forEach(num => {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// });
+
+
+///8///
+// let users = [
+//     {username: 'giorgi', status: false},
+//     {username: 'levani', status: false},
+//     {username: 'anna', status: true}
+// ];
+
+// for (let i = 0; i < users.length; i++) {
+//     if (users[i].status === true) {
+//         console.log(users[i]);
+//         break; 
+//     }
+// }
